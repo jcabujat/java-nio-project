01071d6243dbff21eb75dd95d20b6c258df79ef2
jonathancabujat <jonathancabujat@gmail.com>
2020-10-12 11:21:53 +0800

    03: Added code for writing integers in binary file and how to use pointer and flip() method.

--- src/com/jcabujat/Main.java
+++ src/com/jcabujat/Main.java
@@ -11,8 +11,22 @@ public class Main {
 	    try (FileOutputStream binFile = new FileOutputStream("data.dat");
              FileChannel binChannel = binFile.getChannel()){
 	        byte[] outputBytes = "Hello World!".getBytes();
-            ByteBuffer byteBuffer = ByteBuffer.wrap(outputBytes);
+            ByteBuffer byteBuffer = ByteBuffer.wrap(outputBytes); // this method repoints the pointer to the start of the buffer
             int numBytes = binChannel.write(byteBuffer);
+            System.out.println("Number of bytes written = " + numBytes);
+
+            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+            intBuffer.putInt(245);
+            intBuffer.flip(); // need to reset the pointer at the start before writing.
+            // Note that you need to reset pointer everytime you switch from reading and writing in the buffer.
+            // Writing to the buffer switches to reading the buffer then writing to channel.
+            numBytes = binChannel.write(intBuffer); // this points the pointer to the end of the buffer
+            System.out.println("Number of bytes written = " + numBytes);
+
+            intBuffer.flip(); // need to reset the pointer again before reusing the buffer and putting an int again
+            intBuffer.putInt(-98765);
+            intBuffer.flip(); // always reset before writing except when calling a method that automatically resets the pointer
+            numBytes = binChannel.write(intBuffer);
             System.out.println("Number of bytes written = " + numBytes);
 
         } catch (IOException e) {
