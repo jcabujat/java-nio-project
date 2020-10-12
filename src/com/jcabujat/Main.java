34db1f9e77178e7b89503b4dab197a50b56c29ca
jonathancabujat <jonathancabujat@gmail.com>
2020-10-12 10:56:40 +0800

    02: Writing binary files using java.nio

--- src/com/jcabujat/Main.java
+++ src/com/jcabujat/Main.java
@@ -1,23 +1,20 @@
 package com.jcabujat;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.util.List;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class Main {
 
     public static void main(String[] args) {
-	    try {
-            Path dataPath = FileSystems.getDefault().getPath("data.txt");
-            Files.write(dataPath, "\nLine 5".getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
-            List<String> lines = Files.readAllLines(dataPath);
-            for (String line: lines) {
-                System.out.println(line);
-            }
+	    try (FileOutputStream binFile = new FileOutputStream("data.dat");
+             FileChannel binChannel = binFile.getChannel()){
+	        byte[] outputBytes = "Hello World!".getBytes();
+            ByteBuffer byteBuffer = ByteBuffer.wrap(outputBytes);
+            int numBytes = binChannel.write(byteBuffer);
+            System.out.println("Number of bytes written = " + numBytes);
+
         } catch (IOException e) {
 	        e.printStackTrace();
         }
